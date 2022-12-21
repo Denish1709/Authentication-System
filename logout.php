467e3eaf433de45e81f12708cd67175dce54c745
Denish1709 <denishrao22s@forwardschool.edu.my>
2022-12-21 19:24:29 +0800

    init

---
+++ logout.php
@@ -0,0 +1,18 @@
+<?php
+
+    session_start();
+
+    // make sure the user is logged in
+    if ( isset( $_SESSION['user'] ) ) {
+        // delete the user's session data
+        unset( $_SESSION['user'] );
+        // redirect user back to inde
+        header( 'Location: /');
+        exit;
+    } else {
+        // redirect to login page if user is not logged in
+        header( 'Location: /login.php' );
+        exit;
+    }
+
+?>
